1c957bef8004b920454d916b61130d71beeee647
devilsoonyip <junyao_92@hotmail.com>
2014-06-08 17:05:45 +0800

    2nd version

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 03, 2014 at 06:08 PM
+-- Generation Time: Jun 08, 2014 at 11:02 AM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -19,26 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `database`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `lecturer`
---
-
-CREATE TABLE IF NOT EXISTS `lecturer` (
-  `LecturerID` int(11) NOT NULL,
-  `LecturerName` varchar(50) NOT NULL,
-  `UnitCode` varchar(10) NOT NULL,
-  `Email` varchar(50) NOT NULL,
-  `Faculty` varchar(50) NOT NULL,
-  `Department` varchar(50) NOT NULL,
-  `Position` varchar(50) NOT NULL,
-  `PhoneNumber` varchar(11) NOT NULL,
-  `Status` tinyint(1) NOT NULL,
-  `NumberOfStudents` int(100) NOT NULL,
-  PRIMARY KEY (`LecturerID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -59,14 +39,16 @@ CREATE TABLE IF NOT EXISTS `login` (
 
 CREATE TABLE IF NOT EXISTS `unit file` (
   `Unit Code` varchar(10) NOT NULL,
-  `Unit Name` varchar(50) NOT NULL,
-  `Location of directory` longtext NOT NULL,
-  `Date of uploaded` date NOT NULL,
-  `Date of modified` date NOT NULL,
-  `Status of approval` tinyint(1) NOT NULL,
+  `Unit Name` varchar(100) NOT NULL,
+  `Location of Directory` varchar(5000) NOT NULL,
+  `Date of Uploaded` date NOT NULL,
+  `Date of Modified` date NOT NULL,
+  `Status of Approval` tinyint(1) NOT NULL DEFAULT '0',
   `Lecturer ID` varchar(10) NOT NULL,
-  `Lecturer name` varchar(50) NOT NULL,
+  `Lecturer Name` varchar(100) NOT NULL,
+  `Lecturer Email` varchar(50) NOT NULL,
   `Total No. of Files` int(100) NOT NULL,
+  `Total No. of Students` int(100) NOT NULL,
   PRIMARY KEY (`Unit Code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
