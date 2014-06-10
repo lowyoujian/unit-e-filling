4c18943408e16a21808acdccab78f53cba575b96
devilsoonyip <junyao_92@hotmail.com>
2014-06-10 12:38:27 +0800

    3rd version

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 08, 2014 at 11:02 AM
+-- Generation Time: Jun 10, 2014 at 06:35 AM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -38,18 +38,15 @@ CREATE TABLE IF NOT EXISTS `login` (
 --
 
 CREATE TABLE IF NOT EXISTS `unit file` (
-  `Unit Code` varchar(10) NOT NULL,
-  `Unit Name` varchar(100) NOT NULL,
-  `Location of Directory` varchar(5000) NOT NULL,
-  `Date of Uploaded` date NOT NULL,
-  `Date of Modified` date NOT NULL,
-  `Status of Approval` tinyint(1) NOT NULL DEFAULT '0',
-  `Lecturer ID` varchar(10) NOT NULL,
-  `Lecturer Name` varchar(100) NOT NULL,
-  `Lecturer Email` varchar(50) NOT NULL,
-  `Total No. of Files` int(100) NOT NULL,
-  `Total No. of Students` int(100) NOT NULL,
-  PRIMARY KEY (`Unit Code`)
+  `UnitCode` varchar(10) NOT NULL,
+  `UnitName` varchar(100) NOT NULL,
+  `Lecturer` varchar(100) NOT NULL,
+  `File Name` varchar(100) NOT NULL,
+  `Last Uploaded` date NOT NULL,
+  `File Status` tinyint(1) NOT NULL,
+  `Year/Trimester` varchar(10) NOT NULL,
+  `HOD in Charge` varchar(100) NOT NULL,
+  PRIMARY KEY (`UnitCode`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
