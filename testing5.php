e400abd09b154b14cc245bb21cfe169ca15f60e2
KokLip <lipzai0625@gmail.com>
2014-07-13 10:33:22 +0800

    approve and update filestatus with ajax function done but interface hvnt complete

---
+++ testing5.php
@@ -0,0 +1,22 @@
+<?php
+$server="localhost";
+  $user="root";
+  $pass="";
+  $database="team_project";
+  
+$t = $_POST['t'];
+
+$link = mysql_connect($server,$user,$pass);
+if (!$link) {
+die('Could not connect: ' . mysql_error());
+}
+$db_selected = mysql_select_db($database, $link);
+
+// Insert the data
+$query2="UPDATE unitFile
+SET fileStatus=1
+WHERE fileName='$t'";
+$results2 = mysql_query($query2, $link);
+echo "$t";
+
+?>
