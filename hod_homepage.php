240a9f4f1c10bf78af480a0a29c1de1a0f21957b
KokLip <lipzai0625@gmail.com>
2014-08-02 12:43:43 +0800

    naming lecturerid to loginid, easier to recognise

--- hod_homepage.php
+++ hod_homepage.php
@@ -9,7 +9,7 @@ $mysqli = new mysqli('localhost', 'root', '', 'team_project');
 					
 	$stmt=$mysqli->prepare("SELECT unitcode,unitdesc FROM hod WHERE hodid =?");
 	$stmt->bind_param('s',
-		$_SESSION['lecturerid']);
+		$_SESSION['loginid']);
 	$stmt->execute();
 	$stmt->bind_result($unitcode,$unitname);
 	
