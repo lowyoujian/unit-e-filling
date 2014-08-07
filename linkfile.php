6605766a278661237300ffe7df8af05102b1d889
LeeQian <iffinityrobugreport@hotmail.com>
2014-08-08 02:02:38 +0800

    removed testing4,5,6.php

--- linkfile.php
+++ linkfile.php
@@ -1,20 +1,127 @@
 <?php
+$approvelist= $_GET['approvelist'];
 
 //For counting number of files in folder
-$directory = "C:/wamp/www/Testing/upload/";
+$directory = "upload/$approvelist/May2014/"; 
 $filecount = count(glob($directory . "*.pdf"));
 
 //For finding .pdf files in the folder
-$phpfile = "C:/wamp/www/Testing/upload/";
+$phpfile = "upload/$approvelist/May2014/";
 $phpfiles = glob($phpfile. "*.pdf");
-
-
+$a=0;
 foreach($phpfiles as $phpfile)
 {
 $name=basename($phpfile);
-echo "<a href='upload/$name' download>".basename($phpfile,".pdf")."</a>";
-echo "<br/>";
+$filehref[$a]='upload/$approvelist/May2014/$name';
+$filename[$a]=$name;
+$a++;
 }
 
-?>
+?>	
+	<html>
+	<body>
+		<fieldset>
+		<legend><?php echo $approvelist;?></legend>
+		
+			
+<?php
+include('database_config.php');
+	session_start();
+	$mysqli = new mysqli($database['ip'], $database['username'], '', $database['database_name']);
+	
+			if ($mysqli->connect_error) {
+				die('Connect Error (' . $mysqli->connect_errno . ') '
+						. $mysqli->connect_error);
+			}
+					
+	$stmt=$mysqli->prepare("SELECT unitcode,semester FROM unitFile");	
+	$stmt->execute();
+	$stmt->bind_result($uc,$sem);
+		
+	for($j=0;$j<$filecount ; $j++){
+	
+				echo "<div id='c' style='margin-bottom:5px;'>";				
+				echo "<a href='$filehref[$j]' id='fileName' name='fileName'>".$filename[$j]."</a>";
+				echo "<div id='box[$j]' class='box' style='position:relative; float:right; left:5px; width:20px; height:20px; background-color:black; '></div>";					
+				echo "<input class='disapprove' type='submit' value='Disapprove' id='disapprove[$j]' name='dis[$j]' style='position:relative; float:right;'/>";				
+				echo "<input class='approve' type='submit' value='Approve' id='approve[$j]' name='app[$j]' style='position:relative; float:right;'/> </div>";
+					
+				
+				
+				
+			}
 
+?>
+	Total number of files : <?php echo $filecount;?><br/>
+	
+	</fieldset>
+	</body>
+	</html>
+	
+<script src="jquery-2.1.1.js"></script>
+<script type="text/javascript">
+
+$(document).ready(function () { 
+$(document.getElementById("approve[0]")).click(function(){
+var ths = this;
+var str = $(ths).siblings("#fileName").attr("href");
+$.post("approve.php", {t:str}, function(value){
+document.getElementById("box[0]").style.backgroundColor="#33FF33";
+
+});
+});
+});
+
+$(document).ready(function () { 
+$(document.getElementById("disapprove[0]")).click(function(){
+var ths = this;
+var str = $(ths).siblings("#fileName").attr("href");
+$.post("disapprove.php", {t:str}, function(value){
+document.getElementById("box[0]").style.backgroundColor="#FF0000";
+});
+});
+});
+
+$(document).ready(function () { 
+$(document.getElementById("approve[1]")).click(function(){
+var ths = this;
+var str = $(ths).siblings("#fileName").attr("href");
+$.post("approve.php", {t:str}, function(value){
+document.getElementById("box[1]").style.backgroundColor="#33FF33";
+
+});
+});
+});
+
+$(document).ready(function () { 
+$(document.getElementById("disapprove[1]")).click(function(){
+var ths = this;
+var str = $(ths).siblings("#fileName").attr("href");
+$.post("disapprove.php", {t:str}, function(value){
+document.getElementById("box[1]").style.backgroundColor="#FF0000";
+});
+});
+});
+
+$(document).ready(function () { 
+$(document.getElementById("approve[2]")).click(function(){
+var ths = this;
+var str = $(ths).siblings("#fileName").attr("href");
+$.post("approve.php", {t:str}, function(value){
+document.getElementById("box[2]").style.backgroundColor="#33FF33";
+
+});
+});
+});
+
+$(document).ready(function () { 
+$(document.getElementById("disapprove[2]")).click(function(){
+var ths = this;
+var str = $(ths).siblings("#fileName").attr("href");
+$.post("disapprove.php", {t:str}, function(value){
+document.getElementById("box[2]").style.backgroundColor="#FF0000";
+});
+});
+});
+
+</script>
--- unitcodelist.php
+++ unitcodelist.php
@@ -18,29 +18,6 @@
 	$stmt2->bind_result($unitcode,$unitname);
 	?>
 </head>
-<script type="text/javascript">
-	function reqListener () {
-		console.log(this.responseText);
-	}
-
-    var oReq = new XMLHttpRequest(); //New request object
-    oReq.onload = function() {
-        //This is where you handle what to do with the response.
-        //The actual data is found on this.responseText
-        //The data from get_hod_status, 1= is a hod, 0= not hod
-        if(this.responseText){
-        	$("#approvelist").append("Here are the list of files for you to verify<br><br>");
-        	$("#approvelist").append("Unit Code List: ");
-        	$("#approvelist").append("<select name='approvelist'><option value='unitcode'</option></select><input type='submit' value='next'/> ");
-
-        }
-    };
-    oReq.open("get", "get_hod_status.php", true);
-    //                               ^ Don't block the rest of the execution.
-    //                                 Don't wait until the request finishes to 
-    //                                 continue.
-    oReq.send();
-    </script>
 <body>
 	<form action="home.php" method="GET">
 		<p> Please select the unit that you wish to upload files. </p>
@@ -56,8 +33,46 @@
 		</select>
 		<input type="submit" value="Next"/>
 	</form>
+<?php
+	$stmt3=$mysqli->prepare("SELECT hodunitcode,hodunitdesc FROM lecturer WHERE lecturerid =?");
+	$stmt3->bind_param('s',
+		$_SESSION['lecturerid']
+		);
+	$stmt3->execute();
+	$stmt3->bind_result($hodunitcode,$hodunitname);
+	
+?>
+<script type="text/javascript">
+	function reqListener () {
+		console.log(this.responseText);
+	}
+
+    var oReq = new XMLHttpRequest(); //New request object
+    oReq.onload = function() {
+        //This is where you handle what to do with the response.
+        //The actual data is found on this.responseText
+        //The data from get_hod_status, 1= is a hod, 0= not hod
+        if(this.responseText==1){
+			$("#approvelist").append("<fieldset><legend>HOD</legend><form action='linkfile.php' method='GET'>Here are the list of files for you to verify. Please select unit.<br>Unit Code List: <select name='approvelist'><?php 
+			while($stmt3->fetch()){
+				if($hodunitcode!='')
+				echo "<option value='$hodunitcode'>$hodunitcode $hodunitname</option>";	 
+			}	
+			?></select><input type='submit' value='Next'/> ");
+			$("#approvelist").append("</form></fieldset>");
+        }
+    };
+    oReq.open("get", "get_hod_status.php", true);
+    //                               ^ Don't block the rest of the execution.
+    //                                 Don't wait until the request finishes to 
+    //                                 continue.
+    oReq.send();
+</script>
+
 	<div id="approvelist">
-	</div>
+
+	</div
+		
 </body>
 
 </html>
