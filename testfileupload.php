fd896f02bfe827768f70816db4ab5ba33f596084
YouJian <lowyoujian@gmail.com>
2014-08-07 23:41:37 +0800

    old pdf file encoding not working

--- testfileupload.php
+++ testfileupload.php
@@ -1,12 +1,11 @@
 <html>
 <body>
 
-<form action="testfileuploadupload.php" method="post"
-enctype="multipart/form-data">
-<label for="file">Filename:</label>
-<input type="file" name="file" id="file"><br>
-<input type="button" name="submit" value="Submit">
-</form>
+	<form action="testfileuploadupload.php" method="post" enctype="multipart/form-data">
+		<label for="file">Filename:</label>
+		<input type="file" name="file" id="file"><br>
+		<button type="submit">Upload</button>
+	</form>
 
 </body>
 </html>
