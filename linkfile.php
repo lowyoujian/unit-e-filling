38cb54e2aaf4453cf172a2c019db7dd20dc22fcf
LeeQian <iffinityrobugreport@hotmail.com>
2014-08-11 00:02:15 +0800

    haven't link from unitcodelist/mod_homepage ?

--- linkfile.php
+++ linkfile.php
@@ -1,4 +1,17 @@
 <?php
+include('database_config.php');
+	session_start();
+	$mysqli = new mysqli($database['ip'], $database['username'], '', $database['database_name']);
+	
+			if ($mysqli->connect_error) {
+				die('Connect Error (' . $mysqli->connect_errno . ') '
+						. $mysqli->connect_error);
+			}
+					
+	$stmt=$mysqli->prepare("SELECT unitcode,semester FROM unitFile");	
+	$stmt->execute();
+	$stmt->bind_result($uc,$sem);
+	
 $approvelist= $_GET['approvelist'];
 
 //For counting number of files in folder
@@ -12,45 +25,45 @@ $a=0;
 foreach($phpfiles as $phpfile)
 {
 $name=basename($phpfile);
-$filehref[$a]='upload/$approvelist/May2014/$name';
+$filehref[$a]="upload/$approvelist/May2014/$name";
 $filename[$a]=$name;
 $a++;
 }
 
 ?>	
 	<html>
+	<style>
+table,th,td
+{
+border:1px solid black;
+border-collapse:collapse;
+}
+th,td
+{
+padding:5px;
+}
+	</style>
 	<body>
 		<fieldset>
 		<legend><?php echo $approvelist;?></legend>
 		
 			
 <?php
-include('database_config.php');
-	session_start();
-	$mysqli = new mysqli($database['ip'], $database['username'], '', $database['database_name']);
-	
-			if ($mysqli->connect_error) {
-				die('Connect Error (' . $mysqli->connect_errno . ') '
-						. $mysqli->connect_error);
-			}
-					
-	$stmt=$mysqli->prepare("SELECT unitcode,semester FROM unitFile");	
-	$stmt->execute();
-	$stmt->bind_result($uc,$sem);
-		
-	for($j=0;$j<$filecount ; $j++){
-	
-				echo "<div id='c' style='margin-bottom:5px;'>";				
-				echo "<a href='$filehref[$j]' id='fileName' name='fileName'>".$filename[$j]."</a>";
-				echo "<div id='box[$j]' class='box' style='position:relative; float:right; left:5px; width:20px; height:20px; background-color:black; '></div>";					
-				echo "<input class='disapprove' type='submit' value='Disapprove' id='disapprove[$j]' name='dis[$j]' style='position:relative; float:right;'/>";				
-				echo "<input class='approve' type='submit' value='Approve' id='approve[$j]' name='app[$j]' style='position:relative; float:right;'/> </div>";
-					
-				
-				
-				
-			}
 
+		
+				echo "<table border='1'><tr>";
+				echo "<tr><th>File</th><th>Approve</th><th>Disapprove</th><th>Status</th></tr>";
+	for($j=0;$j<$filecount ; $j++){
+				
+				echo "<div id='c' style='margin-bottom:5px;'>";				
+				echo "<td><a href='$filehref[$j]' id='fileName' name='fileName' download>".$filename[$j]."</a></td>";																	
+				echo "<td><input class='approve' type='submit' value='Approve' id='approve[$j]'/></td>";
+				echo "<td><input class='disapprove' type='submit' value='Disapprove' id='disapprove[$j]'/></div></td>";	
+				echo "<td><div id='box[$j]' class='box' style='width:20px; height:20px; background-color:black; '></div></td></tr>";
+				
+				
+			}
+				echo "</table>";
 ?>
 	Total number of files : <?php echo $filecount;?><br/>
 	
@@ -58,70 +71,38 @@ include('database_config.php');
 	</body>
 	</html>
 	
+
+	
+<?php for($i=0;$i<$filecount;$i++)
+{
+//Loop the function for every buttons
+?>
 <script src="jquery-2.1.1.js"></script>
 <script type="text/javascript">
 
 $(document).ready(function () { 
-$(document.getElementById("approve[0]")).click(function(){
+$(document.getElementById("approve[<?php echo $i;?>]")).click(function(){
 var ths = this;
 var str = $(ths).siblings("#fileName").attr("href");
 $.post("approve.php", {t:str}, function(value){
-document.getElementById("box[0]").style.backgroundColor="#33FF33";
+document.getElementById("box[<?php echo $i;?>]").style.backgroundColor="#33FF33";
 
 });
 });
 });
 
 $(document).ready(function () { 
-$(document.getElementById("disapprove[0]")).click(function(){
+$(document.getElementById("disapprove[<?php echo $i;?>]")).click(function(){
 var ths = this;
 var str = $(ths).siblings("#fileName").attr("href");
 $.post("disapprove.php", {t:str}, function(value){
-document.getElementById("box[0]").style.backgroundColor="#FF0000";
-});
-});
-});
-
-$(document).ready(function () { 
-$(document.getElementById("approve[1]")).click(function(){
-var ths = this;
-var str = $(ths).siblings("#fileName").attr("href");
-$.post("approve.php", {t:str}, function(value){
-document.getElementById("box[1]").style.backgroundColor="#33FF33";
-
-});
-});
-});
-
-$(document).ready(function () { 
-$(document.getElementById("disapprove[1]")).click(function(){
-var ths = this;
-var str = $(ths).siblings("#fileName").attr("href");
-$.post("disapprove.php", {t:str}, function(value){
-document.getElementById("box[1]").style.backgroundColor="#FF0000";
-});
-});
-});
-
-$(document).ready(function () { 
-$(document.getElementById("approve[2]")).click(function(){
-var ths = this;
-var str = $(ths).siblings("#fileName").attr("href");
-$.post("approve.php", {t:str}, function(value){
-document.getElementById("box[2]").style.backgroundColor="#33FF33";
-
-});
-});
-});
-
-$(document).ready(function () { 
-$(document.getElementById("disapprove[2]")).click(function(){
-var ths = this;
-var str = $(ths).siblings("#fileName").attr("href");
-$.post("disapprove.php", {t:str}, function(value){
-document.getElementById("box[2]").style.backgroundColor="#FF0000";
+document.getElementById("box[<?php echo $i;?>]").style.backgroundColor="#FF0000";
 });
 });
 });
 
 </script>
+
+<?php 
+}
+?>
