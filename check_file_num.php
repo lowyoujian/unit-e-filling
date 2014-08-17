8e5f95ecf9556a364d06c831446d582b4c3214f2
KokLip <lipzai0625@gmail.com>
2014-08-17 15:05:15 +0800

    fixed trimester problem

--- check_file_num.php
+++ check_file_num.php
@@ -10,20 +10,22 @@ function check(){
 var_dump($_POST);
 $code = $_POST['unit_code'];
 include('database_config.php');
+$getDate = new CurrentTrimester();
+$getDate->getCurrentTrimester();
 	$mysqli = new mysqli($database['ip'], $database['username'], '', $database['database_name']);
 	if ($mysqli->connect_error) {
 		die('Connect Error (' . $mysqli->connect_errno . ') '
 			. $mysqli->connect_error);
 	}
-	$stmt=$mysqli->prepare("SELECT num_lecture, num_tutorial, num_practical, num_assignment, num_test, num_quiz FROM lecturer_and_unit_files WHERE unit_code=?");
+	$stmt=$mysqli->prepare("SELECT trimester, num_lecture, num_tutorial, num_practical, num_assignment, num_test, num_quiz FROM lecturer_and_unit_files WHERE unit_code=?");
 	$stmt->bind_param('s', $_POST['unit_code']);
 	$stmt->execute();
-	$stmt->bind_result($lectures, $tutorials, $practicals, $assignments, $tests, $quizes);
+	$stmt->bind_result($trimester, $lectures, $tutorials, $practicals, $assignments, $tests, $quizes);
 ?>
 
 <?php
 while($stmt->fetch()){
-	if($lectures == NULL && $tutorials == NULL && $practicals == NULL && $assignments == NULL && $tests == NULL && $quizes == NULL ){
+	if($lectures == NULL && $tutorials == NULL && $practicals == NULL && $assignments == NULL && $tests == NULL && $quizes == NULL && $trimester == $getDate->date ){
 	header("Location:set_file.php?unit_code=$code");
 	}
 }
@@ -32,5 +34,38 @@ while($stmt->fetch()){
 </script>
 </head>
 <body>
+<?php
+class CurrentTrimester {
+
+
+    function getCurrentTrimester(){
+
+        $date=date('M');
+
+        if($date=="Oct"
+            ||"Nov"
+            ||"Dec"
+            )
+            {$this->date="Oct".date('Y');}
+
+        if($date=="Jan"
+            ||"Feb"
+            ||"Mac"
+            ||"Apr"
+            ||"May"
+            )
+            {$this->date="Jan".date('Y');}
+
+        if($date=="Jun"
+            ||"Jul"
+            ||"Aug"
+            ||"Sept"
+            )
+            {$this->date="May".date('Y');}
+
+        
+    }
+	}
+?>
 </body>
 </html>
