fc28fbdf8922aa81a8ba3c80796ee3660522353e
LeeQian <iffinityrobugreport@hotmail.com>
2014-08-21 03:16:02 +0800

    modified navigation bar

--- linkfile.php
+++ linkfile.php
@@ -1,4 +1,12 @@
-<?php include 'title_bar.php'; ?>
+<link rel="stylesheet" href="css/bootstrap.min.css"/>
+<div class="container">
+	<table>
+		<tr>
+			<td align='center'><a href='mod_homepage.php' class="btn btn-default">Home</a></td>			
+			<td align='center'><a href="logout.php" class="btn btn-default">Logout</a></td>
+		</tr>
+	</table>
+</div>
 	<div class="container">
 		<div class="panel panel-default">
 			<div class="panel-heading">File Verifications</div>
